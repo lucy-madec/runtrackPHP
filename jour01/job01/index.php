39586f6396f6bc4966b073cc0442134c556ecdfd
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-05 10:22:45 +0200

    Ajout du fichier index.php avec des exemples de manipulation de variables

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+// Première partie : Afficher "LaPlateforme"
+$plateforme = "LaPlateforme";
+echo $plateforme . "<br>";
+
+// Deuxième partie : Concaténer et afficher "Vive LaPlateforme !"
+$vive = "Vive";
+$exclamation = "!";
+echo $vive . " " . $plateforme . " " . $exclamation . "<br>";
+
+// Troisième partie : Manipulation d'une variable numérique
+$number = 6;
+echo $number . "<br>";
+$number += 4;
+echo $number . "<br>";
+
+// Quatrième partie : Manipulation d'une variable booléenne
+$status = true;
+echo $status . "<br>";
+$status = false;
+echo $status . "<br>";
+?>
