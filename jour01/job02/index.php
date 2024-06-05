2078b697cd9ffce892e34f54cf23eebf7be59837
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-05 10:50:28 +0200

    Ajout du fichier index.php avec deux méthodes de commentaires

---
+++ jour01/job02/index.php
@@ -0,0 +1,9 @@
+<?php
+// Première méthode de commentaire : commentaire sur une seule ligne
+echo "Hello all ! <br>";
+
+/* 
+    Deuxième méthode de commentaire : commentaire sur plusieurs lignes 
+*/
+echo "Hello all ! <br>";
+?>
