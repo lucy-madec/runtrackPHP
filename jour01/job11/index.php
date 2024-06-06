7c4792a2136bec2b3889a7ced1dca08e50773808
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-06-06 13:20:07 +0200

    Ajout de la fonction getHello() pour retourner et afficher 'Hello LaPlateforme!'

---
+++ jour01/job11/index.php
@@ -0,0 +1,9 @@
+<?php
+// Définition de la fonction getHello
+function getHello() {
+    return "Hello LaPlateforme!";
+}
+
+// Appel de la fonction getHello et affichage de sa valeur de retour
+echo getHello();
+?>
